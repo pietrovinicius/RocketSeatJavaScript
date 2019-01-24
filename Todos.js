dd17e30eb0921cb5fd69e4f80eb74a95109e6774
pietro lima <pietro.lima@hotmail.com>
2019-01-24 13:53:12 -0200

    Exibindo a list

--- Todos.js
+++ Todos.js
@@ -1,1 +1,28 @@
 console.log('Loading --- Todos.js');
+
+//query selector para pegar os elementos do DOM
+
+let listElement = document.querySelector('#app ul');
+let inputElement = document.querySelector('#app input');
+let buttonElement = document.querySelector('#app button');
+
+//list de todos
+let todos = [
+    'Fazer café',
+    'Estudar JavaScript',
+    'Pesquisar na comunidade',
+];
+
+//mostrar os todos na tela 
+function renderTodos() {
+    for (todo of todos) {
+        let todoElement = document.createElement('li');
+        let todoText = document.createTextNode(todo);
+
+        todoElement.appendChild(todoText);
+        listElement.appendChild(todoElement);
+    }
+}
+
+renderTodos();
+
